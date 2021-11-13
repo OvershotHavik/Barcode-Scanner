91b30aff32673e73e1ac9276a21e12a90e2f9ae6
OvershotHavik <plavetzky@gmail.com>
2021-11-13 10:26:18 -0500

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Steve Plavetzky on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
